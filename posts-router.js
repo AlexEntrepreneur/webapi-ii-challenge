77db608ed87ae59c4f4cb0384d9e6611a4ac8b5d
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-19 15:32:56 +0000

    Implement GET Posts Endpoint

--- posts-router.js
+++ posts-router.js
@@ -5,4 +5,13 @@ const router = express.Router();
 
 router.use(express.json());
 
+//====== ENDPOINTS ======//
+router.get('/', (req, res) => {
+  db.find()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({
+      error: "The posts information could not be retrieved."
+    }));
+});
+
 module.exports = router;
